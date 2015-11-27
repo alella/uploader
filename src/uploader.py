c84f0f73d67b7f9b0cd73a56df4f5100a9611d56
alella <ashok.lella@gmail.com>
2015-11-27 10:18:09 +0000

    Takes port from commandline

--- src/uploader.py
+++ src/uploader.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import socket
+import argparse
 
 from flask import *
 from werkzeug import secure_filename
@@ -11,12 +12,27 @@ app.config['UPLOAD_DIR'] = os.getcwd()
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-def get_port():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("",0))
-    s.listen(1)
-    port = s.getsockname()[1]
-    s.close()
+def parse_args():
+    parser = argparse.ArgumentParser(description='Hosts temporary service to upload files.')
+    parser.add_argument('-p',
+                        '--port',
+                        dest='port',
+                        default=False,
+                        help='Host service on this port')
+
+    args = parser.parse_args()
+    return args
+
+def get_port(arg_port):
+    if not arg_port:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("",0))
+        s.listen(1)
+        port = s.getsockname()[1]
+        s.close()
+    else:
+        port = int(arg_port)
+        
     return port
 
 @app.route('/upload', methods=['GET', 'POST'])
@@ -40,8 +56,11 @@ def home():
         </form>"""
 
 if __name__ ==  "__main__":
+    args = parse_args()
     host_ip = socket.gethostbyname(socket.gethostname())
-    port = get_port()
+    port = get_port(args.port)
+    
     if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
         print("Started service on http://{0}:{1}".format(host_ip, port))
+        
     app.run(host='0.0.0.0', debug=True, port=port)
