d9543acf8ce6242e323a829a32fd0e9af11575e7
alella <ashok.lella@gmail.com>
2015-11-27 10:08:32 +0000

    picks up random port now

--- src/uploader.py
+++ src/uploader.py
@@ -1,31 +1,39 @@
-from flask import *
-from werkzeug import secure_filename
 import os
 import logging
 import socket
 
+from flask import *
+from werkzeug import secure_filename
+
 app = Flask(__name__)
-app.config['PORT'] = 50960
 app.config['UPLOAD_DIR'] = os.getcwd()
 
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+def get_port():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("",0))
+    s.listen(1)
+    port = s.getsockname()[1]
+    s.close()
+    return port
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    print "Trying to fetch file..."
+    print("Trying to fetch file...")
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
         to_disk_file_name = os.path.join(app.config['UPLOAD_DIR'], filename)
         file.save(to_disk_file_name)
         
-    print "File saved at {0}".format(to_disk_file_name)
+    print("File saved at {0}".format(to_disk_file_name))
     return "File saved at {0}".format(to_disk_file_name)
 
 @app.route('/')
 def home():
-    print "Service opened in {0}".format(request.remote_addr)
+    print("Service opened in {0}".format(request.remote_addr))
     return """<form method=POST enctype=multipart/form-data action="/upload">
           <p><input type=file name=file>
              <input type=submit value=Upload>
@@ -33,7 +41,7 @@ def home():
 
 if __name__ ==  "__main__":
     host_ip = socket.gethostbyname(socket.gethostname())
+    port = get_port()
     if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
-        print "Started service on http://{0}:{1}".format(host_ip,
-                                                         app.config['PORT'])
-    app.run(host='0.0.0.0', debug=True, port=app.config['PORT'])
+        print("Started service on http://{0}:{1}".format(host_ip, port))
+    app.run(host='0.0.0.0', debug=True, port=port)
