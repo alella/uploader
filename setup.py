879780886b460f6ddda59b23e4bfcee1c6e25c63
alella <ashok.lella@gmail.com>
2015-11-28 03:27:08 +0530

    Adding failsafe for missing ngrok

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "uploader",
-    version = "0.2",
+    version = "1.0",
     package_dir = {'':'src'},
     packages = find_packages('src'),
     scripts = ['src/uploader'],
